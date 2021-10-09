a762674bc4de91a47ff3cbd6447add857ae50de6
Ashutosh Dixit <56062825+ashutosh61973@users.noreply.github.com>
2021-10-09 21:55:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,9 +28,12 @@ app.get('/', (req,res)=>res.send('Hello ashu'));
 
 app.use(errorHandler);
 
-     app.use(express.static(path.join(__dirname, 'build')));
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
   
-    app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  }
+
   
 
 const PORT=process.env.PORT || 5000;
